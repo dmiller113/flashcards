6ad11796b68c5dadc5946e46f1675a63234e5a96
Daniel Miller <danm1113@gmail.com>
2025-11-17 13:52:18 -0800

    Move everything up a level

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-</p>
+<script lang="ts">
+	import { resolve } from '$app/paths';
+
+	type Operation = {
+		href: string;
+		name: string;
+	};
+
+	const operations: ReadonlyArray<Operation> = [
+		{ 
+			name: 'Addition',
+			href: resolve('/cards/[operator=operation]', { operator: 'add' }),
+		},
+		{
+			name: 'Subtraction',
+			href: resolve('/cards/[operator=operation]', { operator: 'subtract' }),
+		},
+		{
+			name: 'Multiplication',
+			href: resolve('/cards/[operator=operation]', { operator: 'multiply' }),
+		},
+		{
+			name: 'Division',
+			href: resolve('/cards/[operator=operation]', { operator: 'divide' }),
+		},
+		{
+			name: 'Mixed Operations',
+			href: resolve('/cards/[operator=operation]', { operator: 'mixed' }),
+		},
+	];
+</script>
+
+<p>Choose operation to review:</p>
+<ul>
+	{#each operations as {href, name}}
+		<li><a {href}>{name}</a></li>
+	{/each}
+</ul>
